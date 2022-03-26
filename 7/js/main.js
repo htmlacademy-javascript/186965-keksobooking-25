3c8f3baf25d75f3718cd9e8e03dc4d93e9d39c27
Keks <keks@htmlacademy.ru>
2022-03-26 17:29:47 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,6 @@
+import './similar-elements.js';
+import {setInactiveFormState, setActiveFormState} from './form-states.js';
+import './form-validation.js';
+
+setInactiveFormState();
+setActiveFormState();
